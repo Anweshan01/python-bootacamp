c420acdc1bafa70dd82849f398a6181bd0ce6cce
Anweshan01 <anweshan330@gmail.com>
2026-07-17 13:13:45 +0000

    Initial commit

---
+++ prime_checker.py
@@ -0,0 +1,19 @@
+#prime Checker
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    is_prime = True
+
+    for i in range(2,num):
+      if num % i == 0:
+        is_prime = False
+        break
+
+    if is_prime:
+        print("Number is prime")
+    else:
+        print("Number is not prime")
+
